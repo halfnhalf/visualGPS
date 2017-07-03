6afb8b249faf75f095f17564978a8dfc883b3112
zrclute <clute-zachary@zai.com>
2017-07-03 15:22:04 -0400

    sync bytes seeking now works. I'm still not entirely convinced i want to store sync bytes as a list of strings since python has the \x escape sequency builtin

--- main.py
+++ main.py
@@ -5,7 +5,10 @@ from visualGPS.reader.Reader import FileReaderController
 def main():
     dir = os.path.dirname(__file__)
     config = os.path.join(dir, 'config', 'config.yml')
-    filereader = FileReaderController("junk", config)
+    datafile = os.path.join(dir, 'data', '1950_5_01_L627_OMNI_0.GPS')
+    filereader = FileReaderController(datafile, config)
+    filereader.get_frame()
+    filereader.get_frame()
 
 if __name__ == "__main__":
     main()
--- visualGPS/reader/Reader.py
+++ visualGPS/reader/Reader.py
@@ -1,22 +1,55 @@
 import yaml
 
+_NUM_BYTES_TO_READ = 1
+
 
 class Reader():
     def __init__(self, configfile):
         self.configfile = configfile
-        self.sync_bytes = yaml.load(open(configfile, 'r'))['sync_bytes']
-        print(self.sync_bytes)
+        self.sync_bytes_list = yaml.load(open(configfile, 'r'))['sync_bytes']
+        self.sync_bytes_list = [bytes.fromhex(x) for x in self.sync_bytes_list]
+        self.current_frame_pos = 0
 
 
 class FileReaderController(Reader):
     """
     This class is used to read binary gps data from a file
-    and parse it according to a config file
+    and deliver frames according to an API
     """
 
     def __init__(self, filename, configfile):
         super(FileReaderController, self).__init__(configfile)
         self.filename = filename
+        self.num_sync_bytes = len(self.sync_bytes_list)
 
-    def get_frame():
-        pass
+    def get_frame(self):
+        """
+        Get frames iteratively, one at a time
+        """
+        with open(self.filename, "rb") as self.binary_file:
+            start_of_frame = self.seek_next_sync_bytes_pos(self.current_frame_pos)
+            end_of_frame = self.seek_next_sync_bytes_pos(start_of_frame+self.num_sync_bytes)
+
+        self.current_frame_pos = end_of_frame
+        print(start_of_frame, end_of_frame)
+
+    def _seek_next_sync_bytes_pos(self, start_position):
+        """
+        this function requires a file stream to already be opened as
+        self.binary_file
+
+        seek and return the start of the next consecutive sync bytes
+        """
+
+        self.binary_file.seek(start_position)
+        bytes_read = self.binary_file.read(1)
+        num_sync_bytes_found = 0
+
+        while bytes_read != b'':
+            if bytes_read == self.sync_bytes_list[num_sync_bytes_found]:
+                num_sync_bytes_found += 1
+                if num_sync_bytes_found == self.num_sync_bytes:
+                    return self.binary_file.tell()-self.num_sync_bytes
+            else:
+                num_sync_bytes_found = 0
+            bytes_read = self.binary_file.read(1)
