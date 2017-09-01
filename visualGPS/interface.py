6e3f06a4a792c3ae49394619751649023c89c818
zrclute <clute-zachary@zai.com>
2017-09-01 14:35:08 -0400

    realtime data parsing and screen refresh is working but the reader needs additional testing

--- visualGPS/interface.py
+++ visualGPS/interface.py
@@ -4,6 +4,7 @@ from asciimatics.scene import Scene
 from asciimatics.exceptions import ResizeScreenError, NextScene
 from collections import defaultdict
 import sys
+import _thread
 import time
 
 last_scene = None
@@ -45,6 +46,7 @@ class HeaderView(Frame):
         self.layout.add_widget(self.payload_data)
         self.layout.add_widget(Divider())
         self.layout.add_widget(Button("next frame", self._next_frame))
+        self.layout.add_widget(Button("Start", self._start_real_time))
         self.fix()
 
         self.palette = defaultdict(
@@ -58,6 +60,20 @@ class HeaderView(Frame):
         self.payload_list.options = [(self.gps_controller.parser.current_frame["message_enum"], 0)]
         self.frame_count_list.options = [(str(self.gps_controller.parser.frame_count), 0)]
         self.payload_data.options = self._convert_dict_to_options(self.gps_controller.parser.current_frame["payload_data"])
+
+    def _start_real_time(self):
+        _thread.start_new_thread(self._real_time, ())
+
+    def _real_time(self):
+        while(True):
+            time.sleep(.5)
+            self.gps_controller.get_frame()
+            self.header_list.options = self._convert_dict_to_options(self.gps_controller.reader.header_structure)
+            self.payload_list.options = [(self.gps_controller.parser.current_frame["message_enum"], 0)]
+            self.frame_count_list.options = [(str(self.gps_controller.parser.frame_count), 0)]
+            self.payload_data.options = self._convert_dict_to_options(self.gps_controller.parser.current_frame["payload_data"])
+            self.screen.force_update()
+
 
     def _convert_dict_to_options(self, dict):
         options = [(field[0] + " : " + str(field[1]), count) for count, field in enumerate(dict.items())]
