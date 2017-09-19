abf06e5aa17584ff4d9995fb9ee7746889831345
zrclute <clute-zachary@zai.com>
2017-09-19 18:36:13 -0400

    figured out how to mix barchat and effects with custom frames

--- visualGPS/interface/__init__.py
+++ visualGPS/interface/__init__.py
@@ -2,7 +2,14 @@ from asciimatics.screen import Screen
 from asciimatics.exceptions import ResizeScreenError, NextScene
 from asciimatics.scene import Scene
 from visualGPS.interface.frame_view import FrameView
-from visualGPS.interface.graph_view import GraphView 
+from visualGPS.interface.graph_view import build_graph_view
+
+def _build_view(screen, scene, gps_controller):
+    scenes = [
+        Scene([FrameView(screen, gps_controller)], -1, name="Frame"),
+        Scene(build_graph_view(screen, gps_controller), -1, name="Graph")
+    ]
+    screen.play(scenes, start_scene=scene)
 
 last_scene = None
 
@@ -14,10 +21,3 @@ def start_tui(gps_controller):
             sys.exit(0)
         except ResizeScreenError as e:
             last_scene = e.scene
-
-def _build_view(screen, scene, gps_controller):
-    scenes = [
-        Scene([FrameView(screen, gps_controller)], -1, name="Frame"),
-        Scene([GraphView(screen, gps_controller)], -1, name="Graph")
-    ]
-    screen.play(scenes, start_scene=scene)
--- visualGPS/interface/graph_view.py
+++ visualGPS/interface/graph_view.py
@@ -1,18 +1,22 @@
 from asciimatics.widgets import Layout, Frame, Divider, Button, ListBox, Label, Widget
 from asciimatics.screen import Screen
 from asciimatics.exceptions import NextScene
+from asciimatics.renderers import BarChart
+from asciimatics.effects import Print
+from asciimatics.renderers import BarChart
 from collections import defaultdict
+
+import time
 
 last_scene = None
 
-class GraphView(Frame):
+class _GraphView(Frame):
     def __init__(self, screen, gps_controller):
-        super(GraphView, self).__init__(screen, screen.height * 2 // 3, screen.width * 2 // 3, title="Visual GPS")
+        super(_GraphView, self).__init__(screen, screen.height * 2 // 3, screen.width * 2 // 3, title="Visual GPS")
         self.gps_controller = gps_controller
         self.layout = Layout([100], fill_frame=True)
         self.add_layout(self.layout)
         self.screen = screen
-
 
         self.header_label = Label("Graph")
         self.layout.add_widget(Button("Back", self._go_to_frame_window))
@@ -32,3 +36,27 @@ class GraphView(Frame):
         options = [(field[0] + " : " + str(field[1]), count) for count, field in enumerate(dict.items())]
 
         return options
+
+def build_graph_view(screen, gps_controller):
+    effects = [
+        _GraphView(screen, gps_controller),
+        Print(screen,
+              BarChart(
+                  7, 60, [lambda: time.time() * 10 % 101],
+                  gradient=[
+                      (33, Screen.COLOUR_RED, Screen.COLOUR_RED),
+                      (66, Screen.COLOUR_YELLOW, Screen.COLOUR_YELLOW),
+                      (100, Screen.COLOUR_WHITE, Screen.COLOUR_WHITE),
+                  ] if screen.colours < 256 else [
+                      (10, 234, 234), (20, 236, 236), (30, 238, 238),
+                      (40, 240, 240), (50, 242, 242), (60, 244, 244),
+                      (70, 246, 246), (80, 248, 248), (90, 250, 250),
+                      (100, 252, 252)
+                  ],
+                  char=">",
+                  scale=100.0,
+                  labels=True,
+                  axes=BarChart.X_AXIS),
+              x=68, y=16, transparent=False, speed=2)
+    ]
+    return effects
