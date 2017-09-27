4b4319d2128a858cbcae1db1d823b0e3c3bf245f
zrclute <zrclute@zai.com>
2017-09-26 20:35:03 -0400

    got the linegraph class to print to screen

--- main.py
+++ main.py
@@ -9,18 +9,18 @@ def main():
     dir = os.path.dirname(__file__)
     config = os.path.join(dir, 'config', 'config.yml')
     datafile = os.path.join(dir, 'data', 'com.log')
-    datafile = os.path.join(dir, 'data', '1950_5_01_L627_OMNI_0.GPS')
-    datafile = '/dev/ttyS0'
-
-    #gps = VisualGPS(config)
-    #gps.add_data_or_port(datafile)
-    #gps.register(ProPak6)
-    #gps.register(FileReaderController)
+    #datafile = os.path.join(dir, 'data', '1950_5_01_L627_OMNI_0.GPS')
+    #datafile = '/dev/ttyS0'
 
     gps = VisualGPS(config)
     gps.add_data_or_port(datafile)
     gps.register(ProPak6)
-    gps.register(SerialReaderController)
+    gps.register(FileReaderController)
+
+    #gps = VisualGPS(config)
+    #gps.add_data_or_port(datafile)
+    #gps.register(ProPak6)
+    #gps.register(SerialReaderController)
 
 #    while True:
 #        input("press a key to get next frame")
--- visualGPS/interface/graph_view.py
+++ visualGPS/interface/graph_view.py
@@ -1,7 +1,7 @@
 from asciimatics.widgets import Layout, Frame, Divider, Button, ListBox, Label, Widget
 from asciimatics.screen import Screen
 from asciimatics.exceptions import NextScene
-from asciimatics.renderers import BarChart
+from asciimatics.renderers import BarChart, DynamicRenderer
 from asciimatics.effects import Print
 from asciimatics.renderers import BarChart
 from collections import defaultdict
@@ -37,26 +37,45 @@ class _GraphView(Frame):
 
         return options
 
+class LineGraph(DynamicRenderer):
+  def __init__(self, height, width):
+    super(LineGraph, self).__init__(height, width)
+
+  def _render_now(self):
+    height = self._height
+    width = self._width
+
+    self._write('#' * 4, 4, 16)
+    return self._plain_image, self._colour_map
+
+#def build_graph_view(screen, gps_controller):
+#    effects = [
+#        _GraphView(screen, gps_controller),
+#        Print(screen,
+#              BarChart(
+#                  7, 60, [lambda: time.time() * 10 % 101],
+#                  gradient=[
+#                      (33, Screen.COLOUR_RED, Screen.COLOUR_RED),
+#                      (66, Screen.COLOUR_YELLOW, Screen.COLOUR_YELLOW),
+#                      (100, Screen.COLOUR_WHITE, Screen.COLOUR_WHITE),
+#                  ] if screen.colours < 256 else [
+#                      (10, 234, 234), (20, 236, 236), (30, 238, 238),
+#                      (40, 240, 240), (50, 242, 242), (60, 244, 244),
+#                      (70, 246, 246), (80, 248, 248), (90, 250, 250),
+#                      (100, 252, 252)
+#                  ],
+#                  char=">",
+#                  scale=100.0,
+#                  labels=True,
+#                  axes=BarChart.X_AXIS),
+#              x=30, y=16, transparent=False, speed=2)
+#    ]
+#    return effects
 def build_graph_view(screen, gps_controller):
     effects = [
         _GraphView(screen, gps_controller),
         Print(screen,
-              BarChart(
-                  7, 60, [lambda: time.time() * 10 % 101],
-                  gradient=[
-                      (33, Screen.COLOUR_RED, Screen.COLOUR_RED),
-                      (66, Screen.COLOUR_YELLOW, Screen.COLOUR_YELLOW),
-                      (100, Screen.COLOUR_WHITE, Screen.COLOUR_WHITE),
-                  ] if screen.colours < 256 else [
-                      (10, 234, 234), (20, 236, 236), (30, 238, 238),
-                      (40, 240, 240), (50, 242, 242), (60, 244, 244),
-                      (70, 246, 246), (80, 248, 248), (90, 250, 250),
-                      (100, 252, 252)
-                  ],
-                  char=">",
-                  scale=100.0,
-                  labels=True,
-                  axes=BarChart.X_AXIS),
-              x=68, y=16, transparent=False, speed=2)
+              LineGraph(100, 100),
+              x=30, y=16, transparent=False, speed=2)
     ]
     return effects
